ba6c7e0abf67036af1ba25c2a43b972855713720
smrutiranjan <smrutiranjan.sahoo@s-force.org>
2019-04-11 01:22:31 +0530

    first mvc completed

---
+++ MVC/First_MVC/src/main/java/com/fmvc/controller/ViewWelcomeController.java
@@ -0,0 +1,18 @@
+package com.fmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ViewWelcomeController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav = null;
+		mav = new ModelAndView();
+		mav.setViewName("welcome");
+		return mav;
+	}
+}
